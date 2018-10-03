d5c9b34b2a89cc67aa8c27180676468f4e314b25
Armand Grundmanis <agrundmanis@inbox.lv>
2018-10-03 23:08:12 +0300

    use summernote partial

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,19 +1,6 @@
 @extends('laracms.dashboard::layouts.app', ['page' => 'Content Management'])
 
-@section('styles')
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
-@endsection
-
-@section('scripts')
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('.summernote').summernote({
-                height: 200
-            });
-        });
-    </script>
-@endsection
+@include('laracms.dashboard::partials.summernote')
 
 @section('content')
     <div class="content">
