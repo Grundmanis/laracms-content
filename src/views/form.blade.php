f116c5e3fa368cc381d4034c9145ffc5d219714e
Armand Grundmanis <agrundmanis@inbox.lv>
2018-10-03 23:01:03 +0300

    Changed texteditor to summernote

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,68 +1,61 @@
-@extends('laracms.dashboard::layouts.app')
+@extends('laracms.dashboard::layouts.app', ['page' => 'Content Management'])
 
 @section('styles')
-
-    <!-- Include external CSS. -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.css">
-
-    <!-- Include Editor style. -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.7.3/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.7.3/css/froala_style.min.css" rel="stylesheet" type="text/css" />
-
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
 @endsection
 
 @section('scripts')
-    <!-- Include external JS libs. -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/mode/xml/xml.min.js"></script>
-
-    <!-- Include Editor JS files. -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.7.3/js/froala_editor.pkgd.min.js"></script>
-
-    <script> $(function() { $('textarea').froalaEditor({
-            enter: $.FroalaEditor.ENTER_BR
-        }) }); </script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.summernote').summernote({
+                height: 200
+            });
+        });
+    </script>
 @endsection
 
 @section('content')
-    <form method="post">
-        {{ csrf_field() }}
+    <div class="content">
+        <div class="container-fluid">
+            <form method="post">
+                {{ csrf_field() }}
 
-        <div class="form-group">
-            <label for="slug">Slug<span>*</span></label>
-            <input value="{{ formValue($content ?? null, 'slug') }}" name="slug" class="form-control" id="slug" placeholder="Slug">
-            <small id="slug" class="form-text text-muted">This slug will be used manually in templates</small>
-        </div>
-
-        <!-- Nav tabs -->
-        <div class="form-group">
-            <ul class="nav nav-tabs" role="tablist">
-                @foreach($locales as $key => $locale)
-                    <li role="presentation" @if(!$key) class="active" @endif>
-                        <a href="#{{ $locale }}" data-toggle="tab">
-                            {{ $locale }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-        <!-- Tab panes -->
-        <div class="tab-content">
-            <label title="value">Value</label>
-
-            @foreach($locales as $key => $locale)
-                <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
-                    <div class="form-group">
-                        <textarea name="{{ $locale }}[value]">
-                            {{ formValue($content ?? null, 'value', $locale) }}
-                        </textarea>
-                    </div>
+                <div class="form-group">
+                    <label for="slug">Slug<span>*</span></label>
+                    <input value="{{ formValue($content ?? null, 'slug') }}" name="slug" class="form-control" id="slug"
+                           placeholder="Slug">
+                    <small id="slug" class="form-text text-muted">This slug will be used manually in templates</small>
                 </div>
-            @endforeach
-        </div>
 
-        <button type="submit" class="btn btn-primary">Save</button>
-    </form>
+                <!-- Nav tabs -->
+                <div class="form-group">
+                    <ul class="nav nav-tabs" role="tablist">
+                        @foreach($locales as $key => $locale)
+                            <li role="presentation" @if(!$key) class="active" @endif>
+                                <a href="#{{ $locale }}" data-toggle="tab">
+                                    {{ $locale }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <label title="value">Value</label>
+                    @foreach($locales as $key => $locale)
+                        <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
+                            <div class="form-group">
+                                <textarea class="summernote" name="{{ $locale }}[value]">
+                                    {{ formValue($content ?? null, 'value', $locale) }}
+                                </textarea>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
 @endsection
