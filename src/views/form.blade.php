2512b18e738365ee29ec90dd3578306ee12d6b10
Armand Grundmanis <agrundmanis@inbox.lv>
2018-10-03 23:44:16 +0300

    removed container

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -3,46 +3,42 @@
 @include('laracms.dashboard::partials.summernote')
 
 @section('content')
-    <div class="content">
-        <div class="container-fluid">
-            <form method="post">
-                {{ csrf_field() }}
+    <form method="post">
+        {{ csrf_field() }}
 
-                <div class="form-group">
-                    <label for="slug">Slug<span>*</span></label>
-                    <input value="{{ formValue($content ?? null, 'slug') }}" name="slug" class="form-control" id="slug"
-                           placeholder="Slug">
-                    <small id="slug" class="form-text text-muted">This slug will be used manually in templates</small>
-                </div>
-
-                <!-- Nav tabs -->
-                <div class="form-group">
-                    <ul class="nav nav-tabs" role="tablist">
-                        @foreach($locales as $key => $locale)
-                            <li role="presentation" @if(!$key) class="active" @endif>
-                                <a href="#{{ $locale }}" data-toggle="tab">
-                                    {{ $locale }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-                <!-- Tab panes -->
-                <div class="tab-content">
-                    <label title="value">Value</label>
-                    @foreach($locales as $key => $locale)
-                        <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
-                            <div class="form-group">
-                                <textarea class="summernote" name="{{ $locale }}[value]">
-                                    {{ formValue($content ?? null, 'value', $locale) }}
-                                </textarea>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-
-                <button type="submit" class="btn btn-primary">Save</button>
-            </form>
+        <div class="form-group">
+            <label for="slug">Slug<span>*</span></label>
+            <input value="{{ formValue($content ?? null, 'slug') }}" name="slug" class="form-control" id="slug"
+                   placeholder="Slug">
+            <small id="slug" class="form-text text-muted">This slug will be used manually in templates</small>
         </div>
-    </div>
+
+        <!-- Nav tabs -->
+        <div class="form-group">
+            <ul class="nav nav-tabs" role="tablist">
+                @foreach($locales as $key => $locale)
+                    <li role="presentation" @if(!$key) class="active" @endif>
+                        <a href="#{{ $locale }}" data-toggle="tab">
+                            {{ $locale }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <label title="value">Value</label>
+            @foreach($locales as $key => $locale)
+                <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
+                    <div class="form-group">
+                        <textarea class="summernote" name="{{ $locale }}[value]">
+                            {{ formValue($content ?? null, 'value', $locale) }}
+                        </textarea>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
+    </form>
 @endsection
--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,39 +1,35 @@
 @extends('laracms.dashboard::layouts.app', ['page' => 'Content Management'])
 
 @section('content')
-    <div class="content">
-        <div class="container-fluid">
-            <div class="form-group">
-                <a class="btn btn-success" href="{{ route('laracms.content.create') }}">Create</a>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Slug</th>
-                        <th>Text</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($contents as $content)
-                        <tr>
-                            <td>{{ $content->id }}</td>
-                            <td>{{ $content->slug }}</td>
-                            <td>{{ $content->value }}</td>
-                            <td>
-                                <a href="{{ route('laracms.content.edit', $content->id) }}">Edit</a>
-                                |
-                                <a onclick="return confirm('Are you sure?')"
-                                   href="{{ route('laracms.content.destroy', $content->id) }}">Delete</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                {{ $contents->links() }}
-            </div>
-        </div>
+    <div class="form-group">
+        <a class="btn btn-success" href="{{ route('laracms.content.create') }}">Create</a>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Slug</th>
+                <th>Text</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($contents as $content)
+                <tr>
+                    <td>{{ $content->id }}</td>
+                    <td>{{ $content->slug }}</td>
+                    <td>{{ $content->value }}</td>
+                    <td>
+                        <a href="{{ route('laracms.content.edit', $content->id) }}">Edit</a>
+                        |
+                        <a onclick="return confirm('Are you sure?')"
+                           href="{{ route('laracms.content.destroy', $content->id) }}">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {{ $contents->links() }}
     </div>
 @endsection
