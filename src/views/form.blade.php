99ed32462be1253610fe8d7158b07e204a1db760
Armand Grundmanis <agrundmanis@inbox.lv>
2019-07-12 22:47:00 +0300

    Updated the template

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -1,44 +1,47 @@
-@extends('laracms.dashboard::layouts.app', ['page' => 'Content Management'])
+@extends(view()->exists('laracms.dashboard.layouts.app') ? 'laracms.dashboard.layouts.app' : 'laracms.dashboard::layouts.app', ['page' => __('laracms::admin.menu.content')] )
 
-@include('laracms.dashboard::partials.summernote')
+{{--@include('laracms.dashboard::partials.summernote')--}}
 
 @section('content')
     <form method="post">
         {{ csrf_field() }}
 
-        <div class="form-group">
-            <label for="slug">Slug<span>*</span></label>
-            <input value="{{ formValue($content ?? null, 'slug') }}" name="slug" class="form-control" id="slug"
-                   placeholder="Slug">
-            <small id="slug" class="form-text text-muted">This slug will be used manually in templates</small>
-        </div>
-
-        <!-- Nav tabs -->
-        <div class="form-group">
-            <ul class="nav nav-tabs" role="tablist">
-                @foreach($locales as $key => $locale)
-                    <li role="presentation" @if(!$key) class="active" @endif>
-                        <a href="#{{ $locale }}" data-toggle="tab">
-                            {{ $locale }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-        <!-- Tab panes -->
-        <div class="tab-content">
-            <label title="value">Value</label>
-            @foreach($locales as $key => $locale)
-                <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
-                    <div class="form-group">
-                        <textarea class="summernote" name="{{ $locale }}[value]">
-                            {{ formValue($content ?? null, 'value', $locale) }}
-                        </textarea>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">{{ __('laracms::admin.menu.content') }}</h4>
+            </div>
+            <div class="card-body">
+                <div class="nav-tabs-navigation">
+                    <div class="nav-tabs-wrapper">
+                        <ul id="tabs" class="nav nav-tabs" role="tablist">
+                            @foreach($locales as $key => $locale)
+                                <li class="nav-item">
+                                    <a class="nav-link @if(!$key) active @endif" data-toggle="tab" href="#{{ $locale }}"
+                                       role="tab" aria-expanded="true">{{ $locale }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
                     </div>
                 </div>
-            @endforeach
+                <div id="my-tab-content" class="tab-content text-center">
+                    @foreach($locales as $key => $locale)
+                        <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}" role="tabpanel"
+                             aria-expanded="true">
+                            <div class="form-group">
+                                <label for="">{{ __('laracms::admin.key') }}</label>
+                                <input value="{{ formValue($content ?? null, 'value', $locale) }}" type="text"
+                                       class="form-control" name="{{ $locale }}[slug]">
+                            </div>
+                            <div class="form-group">
+                                <label for="">{{ __('laracms::admin.text') }}</label>
+                                <textarea class="form-control" name="{{ $locale }}[value]">{{ formValue($content ?? null, 'value', $locale) }}</textarea>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
+        <button type="submit" class="btn btn-primary">{{ __('laracms::admin.save') }}</button>
 
-        <button type="submit" class="btn btn-primary">Save</button>
     </form>
 @endsection
--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,35 +1,57 @@
-@extends('laracms.dashboard::layouts.app', ['page' => 'Content Management'])
+@extends(view()->exists('laracms.dashboard.layouts.app') ? 'laracms.dashboard.layouts.app' : 'laracms.dashboard::layouts.app', ['page' => __('laracms::admin.menu.content')] )
 
 @section('content')
     <div class="form-group">
-        <a class="btn btn-success" href="{{ route('laracms.content.create') }}">Create</a>
+        <a class="btn btn-success" href="{{ route('laracms.content.create') }}">{{ __('laracms::admin.create') }}</a>
     </div>
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Slug</th>
-                <th>Text</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($contents as $content)
-                <tr>
-                    <td>{{ $content->id }}</td>
-                    <td>{{ $content->slug }}</td>
-                    <td>{{ $content->value }}</td>
-                    <td>
-                        <a href="{{ route('laracms.content.edit', $content->id) }}">Edit</a>
-                        |
-                        <a onclick="return confirm('Are you sure?')"
-                           href="{{ route('laracms.content.destroy', $content->id) }}">Delete</a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        {{ $contents->links() }}
+
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{ __('laracms::admin.laracms_users') }}</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class="text-primary">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>{{ __('laracms::admin.key') }}</th>
+                                        <th>{{ __('laracms::admin.text') }}</th>
+                                        <th class="text-right">{{ __('laracms::admin.action') }}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($contents as $content)
+                                    <tr>
+                                        <td>{{ $content->id }}</td>
+                                        <td>{{ $content->slug }}</td>
+                                        <td>{{ $content->value }}</td>
+                                        <td class="text-right">
+
+                                            <a title="{{ __('laracms::admin.edit') }}"
+                                               href="{{ route('laracms.content.edit', $content->id) }}" type="button"
+                                               rel="tooltip" class="btn btn-success btn-icon btn-sm ">
+                                                <i class="fa fa-edit"></i>
+                                            </a>
+                                            <a title="{{ __('laracms::admin.delete') }}"
+                                               onclick="return confirm('{{ __('laracms::admin.sure_to_delete') }}')"
+                                               href="{{ route('laracms.content.destroy', $content->id) }}" type="button"
+                                               rel="tooltip" class="btn btn-danger btn-icon btn-sm ">
+                                                <i class="fa fa-times"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            {{ $contents->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
